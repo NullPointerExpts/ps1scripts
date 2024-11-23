250ea381ac2ec8ea6a63149554a84ab859b27c46
NullPointerExpts <165826373+NullPointerExpts@users.noreply.github.com>
2024-11-23 19:34:21 +0300

    Add files via upload

---
+++ DllsCheck.ps1
@@ -0,0 +1,20 @@
+﻿Clear-Host
+
+$minecraftpid = Read-Host "Enter Minecraft "
+$process = Get-Process -Id $minecraftpid | ForEach-Object { $_.Modules } | Where-Object { $_.ModuleName -like "*.dll" } | Select-Object FileName -ErrorAction SilentlyContinue
+
+foreach($dll in $process) {
+
+    #Write-Host $dll.FileName
+
+    if (-not $dll.FileVersionInfo.FileDescription) {
+
+            $signature = Get-AuthenticodeSignature $dll.FileName
+            
+
+            if ($signature.Status -ne 'Valid') {
+
+                Write-Host "Suspicious DLL: $($dll.FileName)"
+            }
+    }
+}
