c2560264b57ed776c2981fe88f0d94fc20706606
NullPointerExpts <165826373+NullPointerExpts@users.noreply.github.com>
2024-11-23 18:44:51 +0300

    Update DpsAndPcaCheck.ps1

--- DpsAndPcaCheck.ps1
+++ DpsAndPcaCheck.ps1
@@ -1,4 +1,3 @@
-# Определяем имена служб
 Clear-Host
 
 $dps = @("CortexClient:::2023/11/11:12:57:00", "PhasmaClient:::2024/01/10:14:28:11","NoRender:::2023/09/05:20:12:29","AvaloneClient:::2023/05/09:19:10:04","AvaloneGreen:::2023/10/04:16:07:05","AvaloneBlue:::2023/07/03:22:01:11","AnapaV4:::2022/06/07:07:45:55","TakkerProxy:::2022/07/08:23:42:38","UsbCleaner:::2021/11/29:17:36:29","Hider:::2024/01/11:21:51:35", "Blast3xStringRemover:::2022/07/06:20:23:42","R-WipeCleaner:::2024/08/14:18:46:25","ResourceHacker:::2023/11/19:10:07:11","RevoCleaner:::2023/06/06:06:27:09","WiseFolderHider:::2024/03/15:02:10:26","HideFolders:::2023/11/26:21:51:07","Nemezida:::2024/05/31:21:46:54","OceanBypass:::2024/09/29:19:06:37","Hider2:::2024/04/07:19:32:29","WhiteGhostInt:::!2024/11/16:18:35:38","DpsChanger:::!2023/08/28:19:45:41","StringCleaner:::!2024/05/12:10:17:07","StubbornCleaner:::!2023/08/08:13:23:36","Vanish:::!2100/01/16:16:45:26","MicoHitBoxes:::!2024/09/11:23:23:32","MerzlotaCleaner:::!2024/09/08:19:10:30","SizeChanger:::!2024/09/19:16:34:40","Unicorn:::!2076/05/18:04:53:15","FakeJT:::!2066/10/01:22:12:07")
@@ -8,7 +7,7 @@ $pca = @("CortexClient:::0x16ed000","TroxillClient:::0x1b44000","PhasmaClient:::
 Invoke-WebRequest -Uri "https://github.com/ZaikoARG/xxstrings/releases/download/1.0.0/xxstrings64.exe" -OutFile "xxstrings64.exe"
 
 
-# Получаем объект службы по имени
+
 $serviceStatus = Get-Service -Name "DPS" -ErrorAction SilentlyContinue
 
 if ($serviceStatus) {
@@ -16,7 +15,7 @@ if ($serviceStatus) {
     if ($serviceStatus.Status -eq 'Running') {
             
         $serviceProcess = Get-WmiObject Win32_Service | Where-Object { $_.Name -eq "DPS" }
-        Write-Host "Служба 'DPS' включена и работает. PID: $($serviceProcess.ProcessId)"
+        Write-Host "РЎР»СѓР¶Р±Р° DPS Р°РєС‚РёРІРЅР°. PID: $($serviceProcess.ProcessId)"
 
         $output = .\xxstrings64.exe -p $($serviceProcess.ProcessId) | Out-String
         
@@ -25,7 +24,7 @@ if ($serviceStatus) {
                     $parts = $string -split ":::" 
                     
                     if ($out.Contains($parts[1])) {
-                        Write-Host "[DPS] Найден запуск "$parts[0] -ForegroundColor Red
+                        Write-Host "[DPS] РќР°Р№РґРµРЅ Р·Р°РїСѓСЃРє "$parts[0] -ForegroundColor Red
                     }
                }
         }
@@ -33,11 +32,11 @@ if ($serviceStatus) {
         
 
     } else {
-        Write-Host "Служба '$service' не запущена."
+        Write-Host "РЎР»СѓР¶Р±Р° 'DPS' РЅРµ Р·Р°РїСѓС‰РµРЅР°." -ForegroundColor Yellow
     }
 
 } else {
-    Write-Host "Служба '$service' не найдена."
+    Write-Host "РЎР»СѓР¶Р±Р° 'DPS' РЅРµ Р·Р°РїСѓС‰РµРЅР°." -ForegroundColor Yellow
 }
 
 
@@ -52,7 +51,7 @@ if ($serviceStatus) {
     if ($serviceStatus.Status -eq 'Running') {
             
         $serviceProcess = Get-WmiObject Win32_Service | Where-Object { $_.Name -eq "PcaSvc" }
-        Write-Host "Служба 'PcaSvc' включена и работает. PID: $($serviceProcess.ProcessId)"
+        Write-Host "РЎР»СѓР¶Р±Р° DPS Р°РєС‚РёРІРЅР°. PID: $($serviceProcess.ProcessId)"
 
         $output = .\xxstrings64.exe -p $($serviceProcess.ProcessId) | Out-String
         
@@ -61,7 +60,7 @@ if ($serviceStatus) {
                     $parts = $string -split ":::" 
                     
                     if ($out.Contains($parts[1])) {
-                        Write-Host "[PCA] Найден запуск "$parts[0] -ForegroundColor Red
+                        Write-Host "[PCA] РќР°Р№РґРµРЅ Р·Р°РїСѓСЃРє "$parts[0] -ForegroundColor Red
                     }
                }
         }
@@ -69,11 +68,11 @@ if ($serviceStatus) {
         
 
     } else {
-        Write-Host "Служба '$service' не запущена."
+        Write-Host "РЎР»СѓР¶Р±Р° 'PcaSvc' РЅРµ Р·Р°РїСѓС‰РµРЅР°." -ForegroundColor Yellow
     }
 
 } else {
-    Write-Host "Служба '$service' не найдена."
+    Write-Host "РЎР»СѓР¶Р±Р° 'PcaSvc' РЅРµ Р·Р°РїСѓС‰РµРЅР°." -ForegroundColor Yellow
 }
     
 
