884f17fe99970ba7758e3a55cbb4eb9c22248155
NullPointerExpts <165826373+NullPointerExpts@users.noreply.github.com>
2025-01-14 09:04:28 +0300

    Add files via upload

---
+++ JMapParser.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+
+$jmap_path = Read-Host "Enter JMap path: "
+$minecraftpid = Read-Host "Enter Minecraft PID: "
+
+
+$output = (& $jmap_path "-histo" $minecraftpid) -split "`n"
+
+
+foreach($out in $output) {
+    if (-not $out.Contains("com.") -and -not $out.Contains("org.") -and -not $out.Contains("net.") -and -not $out.Contains("java.") -and -not $out.Contains("jdk.") -and -not $out.Contains("sun.") -and -not $out.Contains("io.")) {
+        Write-Host $out
+    }   
+}
