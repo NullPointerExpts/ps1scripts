7efa68c9610f78e4baa407474c4c69a92550b49d
NullPointerExpts <165826373+NullPointerExpts@users.noreply.github.com>
2025-01-14 09:10:01 +0300

    Update JMapParser.ps1

--- JMapParser.ps1
+++ JMapParser.ps1
@@ -2,13 +2,15 @@
 
 $jmap_path = Read-Host "Enter JMap path: "
 $minecraftpid = Read-Host "Enter Minecraft PID: "
-
+$result = @()
 
 $output = (& $jmap_path "-histo" $minecraftpid) -split "`n"
 
 
 foreach($out in $output) {
     if (-not $out.Contains("com.") -and -not $out.Contains("org.") -and -not $out.Contains("net.") -and -not $out.Contains("java.") -and -not $out.Contains("jdk.") -and -not $out.Contains("sun.") -and -not $out.Contains("io.")) {
-        Write-Host $out
+        $result += $out
     }   
 }
+
+$result | Out-File -FilePath "C:\jmap_output.txt" -Encoding UTF8
